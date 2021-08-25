7395c6d0317967316f299e6ca56142ee9305986c
Alexander Rojas <alexrrojas.b@gmail.com>
2021-08-25 10:54:25 -0600

    add visuals and fix design issue

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,10 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var deleteBtn: UIButton!
+    @IBOutlet weak var clearBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        deleteBtn.layer.cornerRadius = 30
+        deleteBtn.layer.maskedCorners = [.layerMaxXMinYCorner]
+        
+        clearBtn.layer.cornerRadius = 30
+        clearBtn.layer.maskedCorners = [.layerMinXMinYCorner]
+        
     }
 
 
